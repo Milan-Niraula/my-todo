a5f50858bb3297e3932b1725e100648b3e3bc13c
Ujjwal Kumar NIraula <106484279+Milan-Niraula@users.noreply.github.com>
2023-07-26 12:38:36 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+let globalID = 0;
 function App() {
+  const [todos, setTodos] = useState([])
+  const [task, setTask] = useState("")
+
+  function createTodo(event) {
+    event.preventDefault()
+    setTodos(oldTodes => {
+      setTask("")
+      return [...oldTodes, {todo: task, id: globalID ++ }]
+    })
+  }
+  function deleteList(itemId){
+    setTodos(oldTodes => oldTodes.filter(item => item.id !== itemId))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-3">
+        <h3>My To Do list</h3>
+        <hr />
+        <form onSubmit={createTodo}>
+          <input type='text' value={task} onChange={e => setTask(e.target.value)} /> &nbsp;
+          <button type="submit" className="btn btn-primary">ADD</button>
+        </form>
+        <ol>
+          {todos.map((item, index) => {
+            return <div key={item.id}>
+               <li>{item.todo} &nbsp;&nbsp;&nbsp;
+               <button className="btn btn-danger" onClick={() => deleteList(item.id)}>Delete</button></li>
+            </div>
+          })}
+        </ol>
+      </div>
+    </>
   );
 }
 
